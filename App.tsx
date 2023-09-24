1c8d4672f26e7f3fd740420ae00b5312c92f8083
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-09-24 16:54:15 -0300

    feat: adicionando o primeiro commit junto com a tela de login

---
+++ App.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <SafeAreaView>
+        <View style={styles.logo}>
+          <Image
+            source={require('./src/assets/img/logo.png')}
+            style={styles.imagem}
+          />
+        </View>
+        <View />
+        <View style={styles.form}>
+          <Text style={styles.mainText}>Login</Text>
+          <View style={styles.input}>
+            <TextInput placeholder="Escreva seu login" />
+          </View>
+          <Text style={styles.mainText}>Senha</Text>
+          <View style={styles.input}>
+            <TextInput placeholder="Escreva sua senha" />
+          </View>
+          <TouchableOpacity
+            onPress={() => console.log('User logado')}
+            style={styles.button}>
+            <Text style={styles.textButton}>Acessar</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  textButton: {
+    color: 'white',
+    textAlign: 'center',
+    flex: 1,
+    flexDirection: 'column',
+    textAlignVertical: 'center',
+  },
+  button: {
+    backgroundColor: '#2E2EFE',
+    width: '60%',
+    height: '12%',
+    marginTop: 30,
+    borderWidth: 2,
+    borderRadius: 16,
+  },
+  input: {
+    backgroundColor: 'white',
+    width: 230,
+    borderWidth: 2,
+    borderRadius: 16,
+    borderColor: 'gray',
+    alignItems: 'flex-start',
+  },
+  mainText: {
+    alignSelf: 'flex-start',
+    paddingLeft: 35,
+    paddingTop: 40,
+  },
+  form: {
+    backgroundColor: '#D8D8D8',
+    width: 300,
+    height: 350,
+    alignSelf: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    display: 'flex',
+    gap: 10,
+    borderWidth: 2,
+    borderRadius: 16,
+    bottom: 30,
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#1ca6f1',
+  },
+  imagem: {
+    width: '100%',
+    height: '40%',
+  },
+});
