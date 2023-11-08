5077c7af5ec23f6f3928bb5c3902036d4a925352
victor <eduardopontesdias@outlook.com>
2023-11-07 21:29:29 -0300

    adicionando tela de logout

--- src/screens/Logout/index.tsx
+++ src/screens/Logout/index.tsx
@@ -1,10 +1,79 @@
 /* eslint-disable prettier/prettier */
-import React, { Text, View } from 'react-native';
+import React from 'react';
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-export default function Logout() {
+export default function LoginScreen() {
   return (
-    <View>
-      <Text>Sair</Text>
+    <View style={styles.mainContainer}>
+        <View style={styles.logo}>
+          <Image source={require('../../assets/img/logo.png')} style={styles.imagem} />
+        </View>
+        <View />
+        <View style={styles.form}>
+          <Text style={styles.mainText}>Voce tem certeza que deseja realmente sair do seu perfil?</Text>
+          <View style={{flex: 1,  flexDirection: 'row',gap: 20, }}>
+            <TouchableOpacity onPress={() => console.log('Voltar')} style={styles.button}>
+              <Text style={styles.textButton}>Voltar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => console.log('Sair')} style={styles.button}>
+              <Text style={styles.textButton}>Sair</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  textButton: {
+    color: 'white',
+    textAlign: 'center',
+    flex: 1,
+    flexDirection: 'column',
+    textAlignVertical: 'center',
+  },
+  button: {
+    backgroundColor: '#1ca6f1',
+    width: 100,
+    height: 30,
+    marginTop: 30,
+    borderWidth: 2,
+    borderRadius: 16,
+    
+  },
+  mainText: {
+    marginTop: 40,
+    textAlign: 'center',
+  },
+  form: {
+    backgroundColor: '#D8D8D8',
+    width: 290,
+    height: 200,
+    alignSelf: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: 30,
+    borderWidth: 2,
+    borderRadius: 16,
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#1ca6f1',
+  },
+  imagem: {
+    width: '100%',
+    height: '40%',
+  },
+});
